721e5465fccfc690b61dbe8abddaf850230a35b6
saichatla <chatla.6@osu.edu>
2022-12-18 17:42:26 -0500

    added creation of hash

---
+++ src/hash_table.h
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_table.h"
+
+static ht_item* ht_new_item(const char* k, const char* v){
+  ht_item* i = mallac(sizeof(ht_item));
+  i->key = strdup(k);
+  i->value = strdup(v);
+  return i;
+}
+
+ht_hash_table* ht_new() {
+  ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+  ht->size = 53;
+  ht->count = 0;
+  ht->items = calloc((size_t)ht->size, sizeof(ht_items*));
+  return ht;
+}
+
+ 
+
+
